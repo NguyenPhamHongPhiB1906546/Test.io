e1dd3c396af1a908264d6ce4ebfbccad3fea44ff
unknown <Hongphi0744@gmail.com>
2022-10-31 19:20:12 +0700

    Dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const contactsRouter = require("./app/routes/contact.route");
 const cors = require("cors");
 const app = express();
 app.use(cors());
@@ -6,4 +7,5 @@ app.use(express.json());
 app.get("/",(req, res)=>{
     res.json({message:"Welcome to contact book application."});
  });
- module.exports = app;
+app.use("/api/contacts",contactsRouter)
+module.exports = app;
