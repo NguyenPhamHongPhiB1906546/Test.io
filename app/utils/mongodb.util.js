0f3884fd83f8a91386df6279b5410d8b7edf8ff9
unknown <Hongphi0744@gmail.com>
2022-11-29 20:57:43 +0700

    Du an web

--- app/utils/mongodb.util.js
+++ app/utils/mongodb.util.js
@@ -1,6 +1,7 @@
-const { MongoClient }=require("mongodb");
-class MongoDB{
-    static connect= async (uri) =>{
+const { MongoClient } = require("mongodb");
+
+class MongoDB {
+    static connect = async (uri) => {
         if (this.client) return this.client;
         this.client = await MongoClient.connect(uri);
         return this.client;
